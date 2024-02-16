64d5eabbd86133e22fbec7acd34b36f972213bf4
pika-pocky <160068753+pika-pocky@users.noreply.github.com>
2024-02-16 11:33:54 -0800

    Create challangeD.rb

---
+++ challangeD.rb
@@ -0,0 +1,138 @@
+use_bpm 136
+use_synth :piano
+define :measure28 do
+  play:E4
+  sleep 1
+  play:E5
+  play:Gs4
+  sleep 2
+  play:E5
+  play:Gs4
+  sleep 1
+end
+define :measure346 do
+  play :r
+  sleep 1
+  play:E5
+  play:Gs4
+  play:B4
+  sleep 1
+  play:B4
+  sleep 1
+  play:E5
+  play:Gs4
+  sleep 1
+end
+define :measure57 do
+  play:B4
+  sleep 0.75
+  play:As4
+  sleep 0.25
+  play:Gs4
+  play:B4
+  sleep 0.75
+  play:Cs5
+  sleep 0.25
+  play:B4
+  sleep 1
+  play:Gs4
+  play:E5
+  sleep 1
+end
+
+live_loop:background_notes do
+  play:E3, amp: 0.25
+  sleep 1
+  play:B3, amp: 0.25
+  play:E4, amp: 0.25
+  sleep 1
+  play:B3, amp: 0.25
+  sleep 1
+  play:E4, amp: 0.25
+  sleep 1
+  
+  play:E3, amp: 0.5
+  sleep 1
+  play:B3, amp: 0.5
+  play:E4, amp: 0.5
+  sleep 1
+  play:B3, amp: 0.5
+  sleep 1
+  play:E4, amp: 0.5
+  sleep 1
+  
+  play:E3, amp: 0.75
+  sleep 1
+  play:B3, amp: 0.75
+  play:E4, amp: 0.75
+  sleep 1
+  play:B3, amp: 0.75
+  sleep 1
+  play:E4, amp: 0.75
+  sleep 1
+  
+  9.times do
+    play:E3, amp: 1
+    sleep 1
+    play:B3, amp: 1
+    play:E4, amp: 1
+    sleep 1
+    play:B3, amp: 1
+    sleep 1
+    play:E4, amp: 1
+    sleep 1
+  end
+  stop
+end
+live_loop :drums do
+  sample :drum_bass_hard
+  sleep 0.5
+end
+live_loop :beat do
+  sample :bd_haus, amp: 1
+  sleep 0.25
+end
+live_loop :beat do
+  sample :bd_klub, amp: 1
+  sleep 2
+end
+
+
+# Measure 1
+play :r
+sleep 1
+play:E4
+play:Gs4
+sleep 2
+play:E5
+play:Gs4
+sleep 1
+
+# Measure 2
+measure28
+
+# Measure 3
+measure346
+
+# Measure 4
+measure346
+
+# Measure 5
+measure57
+
+# Measure 6
+measure346
+
+# Measure 7
+measure57
+
+# Measure 8
+measure28
+
+sample :vinyl_scratch
+sleep 0.5
+sample :vinyl_rewind
+sleep 0.5
+sample :vinyl_hiss
+sleep 0.5
+stop
