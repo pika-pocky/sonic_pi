9fb748f300e1abb1548fde7e1b5cc677f5800009
pika-pocky <160068753+pika-pocky@users.noreply.github.com>
2024-03-01 11:26:16 -0800

    Update custom.rb

--- custom.rb
+++ custom.rb
@@ -1,51 +1,94 @@
 #soreal
+#https://www.youtube.com/watch?v=eVtHMDJcmxE
 x=2
+y=0.1
 harmonica="C:/Users/rafael_angeles/Desktop/If You Wanna Get To Heaven sample shorter.wav"
 live_loop :ham do
-  sample harmonica,beat_stretch: 4, amp: 4
-  sleep 4
+  16.times do
+    sample harmonica,beat_stretch: 4, amp: 4
+    sleep 4
+  end
+  stop
 end
-
-use_synth :dpulse
 live_loop :background1 do
-  play 70
-  sleep 4
+  use_synth :dpulse
+  12.times do
+    with_fx :reverb do
+      play 70, amp: y
+      sleep 4
+      y=y+0.1
+    end
+  end
+  stop
 end
 use_synth :kalimba
 live_loop :background2 do
-  play 30
-  sleep 2
+  16.times do
+    play 30
+    sleep 2
+  end
+  stop
 end
 sleep 4
 use_synth :bass_foundation
 live_loop :background3 do
-  play 21
-  sleep 1
+  40.times do
+    play 21
+    sleep 1
+  end
+  stop
 end
 sleep 4
 use_synth :blade
 live_loop :background4 do
-  play 90
-  sleep 1
+  40.times do
+    play 90
+    sleep 1
+  end
+  stop
 end
 sleep 8
 live_loop :lastone do
-  sample :bd_haus
-  sleep x
-  sample :bass_dnb_f
-  sleep x
+  16.times do
+    sample :bd_haus
+    sleep x
+    sample :bass_dnb_f
+    sleep x
+  end
+  stop
 end
 sleep 8
 x=1
 sleep 8
 x=100000
-live_loop :nvmlastonefr do
-  sample :bd_haus
-  sleep 0.5
-  sample :bd_haus
-  sleep 0.5
-  sample :bass_dnb_f
-  sleep 0.5
-  sample :bass_dnb_f
-  sleep 0.5
+live_loop :B do
+  6.times do
+    sample :bd_haus
+    sleep 0.5
+    sample :bd_haus
+    sleep 0.5
+    sample :bass_dnb_f
+    sleep 0.5
+    sample :bass_dnb_f
+    sleep 0.5
+  end
+  stop
 end
+sleep 4
+live_loop :A do
+  4.times do
+    sample :bd_haus
+    sleep 0.3333
+    sample :bd_haus
+    sleep 0.3333
+    sample :bd_haus
+    sleep 0.3333
+    sample :bass_dnb_f
+    sleep 0.5
+    sample :bass_dnb_f
+    sleep 0.5
+  end
+  stop
+end
+sleep 8
+
