ae3a41a2a6917928e690602b9c76ff0025140e6c
pika-pocky <160068753+pika-pocky@users.noreply.github.com>
2024-03-01 10:27:21 -0800

    Update custom.rb

--- custom.rb
+++ custom.rb
@@ -1,12 +1,51 @@
-live_loop :beat do
-  sample :bd_haus, amp: 1
+#soreal
+x=2
+harmonica="C:/Users/rafael_angeles/Desktop/If You Wanna Get To Heaven sample shorter.wav"
+live_loop :ham do
+  sample harmonica,beat_stretch: 4, amp: 4
+  sleep 4
+end
+
+use_synth :dpulse
+live_loop :background1 do
+  play 70
+  sleep 4
+end
+use_synth :kalimba
+live_loop :background2 do
+  play 30
+  sleep 2
+end
+sleep 4
+use_synth :bass_foundation
+live_loop :background3 do
+  play 21
+  sleep 1
+end
+sleep 4
+use_synth :blade
+live_loop :background4 do
+  play 90
+  sleep 1
+end
+sleep 8
+live_loop :lastone do
+  sample :bd_haus
+  sleep x
+  sample :bass_dnb_f
+  sleep x
+end
+sleep 8
+x=1
+sleep 8
+x=100000
+live_loop :nvmlastonefr do
+  sample :bd_haus
   sleep 0.5
-end
-live_loop :beat2 do
-  sample :loop_amen, beat_stretch: 1.5, amp: 4
-  sleep 1.5
-end
-live_loop :drums do
-  sample :drum_bass_hard
+  sample :bd_haus
+  sleep 0.5
+  sample :bass_dnb_f
+  sleep 0.5
+  sample :bass_dnb_f
   sleep 0.5
 end
